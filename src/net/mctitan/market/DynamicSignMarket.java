ee0cf0b609a5fc8dbece6a8203b61cbc5bf63481
Colin Alexander Barr <colin.a.barr@gmail.com>
2012-07-25 18:36:26 -0400

    updates!!!!

--- src/net/mctitan/market/DynamicSignMarket.java
+++ src/net/mctitan/market/DynamicSignMarket.java
@@ -1,7 +1,27 @@
 package net.mctitan.market;
 
+import java.util.HashMap;
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DynamicSignMarket extends JavaPlugin {
+    private static DynamicSignMarket instance;
+    private HashMap<Material, DynamicItem> items;
     
+    @Override
+    public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        
+        instance = this;
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+    
+    public static DynamicSignMarket getInstance() {
+        return instance;
+    }
 }
